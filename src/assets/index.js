90ebc5848eba7bfae04c76e675e44b52a61c5a88
Caio Luca <caioluca.job@gmail.com>
2023-03-22 18:05:17 -0300

    add directory indexes

---
+++ src/assets/index.js
@@ -0,0 +1,17 @@
+import avatarPNG from './pics/avatar.png'
+import backgroundPNG from './pics/background.png'
+
+export {
+	avatarPNG, 
+	backgroundPNG, 
+}
+
+export { ReactComponent as discordSVG } from './icons/discord.svg'
+export { ReactComponent as githubSVG } from './icons/github.svg'
+export { ReactComponent as spotifySVG } from './icons/spotify.svg'
+export { ReactComponent as twitchSVG } from './icons/twitch.svg'
+export { ReactComponent as twitterSVG } from './icons/twitter.svg'
+export { ReactComponent as youtubeSVG } from './icons/youtube.svg'
+
+export { ReactComponent as mailSVG } from './icons/mail-icon.svg'
+export { ReactComponent as phoneSVG } from './icons/phone-icon.svg'
---
+++ src/styles/index.js
@@ -0,0 +1,2 @@
+export { defaultTheme } from './defaultTheme'
+export { GlobalStyle } from './globalStyle'
