590695c9d31e30c78b89ff0dbae56e99d624fcf7
Caio Luca <caioluca.job@gmail.com>
2023-03-22 12:10:27 -0300

    setup default and global style

---
+++ src/styles/defaultTheme.js
@@ -0,0 +1,16 @@
+export { GlobalStyle } from './globalStyle'
+
+export const defaultTheme = {
+	dark: {
+		primary: '#000000',
+		secondary: '#515254',
+		tertiary: '#858587',
+		quartiary: '#51525470', // rgba(81,82,84,0.7)
+	},
+	light: {
+		primary: '#FFFFFF',
+		secondary: '#FBFBFB',
+		tertiary: '#F2F5FA',
+		quartiary: '#F1F5FB',
+	}
+}
---
+++ src/styles/globalStyle.js
@@ -0,0 +1,25 @@
+import { createGlobalStyle } from 'styled-components'
+
+export const GlobalStyle = createGlobalStyle`
+	* {
+		padding: 0px;
+		margin: 0px;
+		box-sizing: border-box;
+	}
+
+	html, body, #root {
+		width: 100%;
+		height: 100%;
+	}
+
+	body {
+		font-family: 'Poppins', sans-serif;
+		background-color: ${({ theme }) => theme.light.tertiary};
+		padding: 14px 28px;
+	}
+
+	#root {
+		display: flex;
+		justify-content: center;
+	}
+`
